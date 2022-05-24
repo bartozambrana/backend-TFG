2466d96341e77364e385c036ceee8838dc8904ee
Barto Zambrana <bartolomezambranaperez99@gmail.com>
2022-05-24 22:46:24 +0200

    home user post random finished

--- controllers/user.js
+++ controllers/user.js
@@ -1,133 +1,200 @@
 /** requirements - thrid party **/
-const {response,request} = require('express');
-const bcryptjs = require('bcryptjs');
+const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
 
 /** Local requirements **/
 const User = require("../models/users");
 const Service = require("../models/services");
-const deleteServiceElements = require('../helpers/deleteService');
+const deleteServiceElements = require("../helpers/deleteService");
+const Posts = require("../models/posts");
+const Works = require("../models/works");
+const { default: mongoose } = require("mongoose");
 
+const getUser = async (req = request, res = response) => {
+  try {
+    //realiza la petición el mismo que quiere borrarlo.
+    const user = await User.findById(req.uid).populate({
+      path: "followServices",
+      select: "serviceName",
+    });
+    let services = undefined;
 
+    if (user.type) services = await Service.find({ idUser: req.uid });
 
+    if (!services)
+      return res.json({
+        user,
+        success: true,
+      });
 
-const getUser = async(req = request, res = response) =>{
-
-    
-    try{ //realiza la petición el mismo que quiere borrarlo.
-        const user = await User.findById(req.uid).populate({path:'followServices',select:'serviceName'});
-        let services = undefined;
-        
-        if(user.type)
-             services = await Service.find({idUser:req.uid});
-        
-        if(!services)
-            return res.json({
-                user,
-                success: true
-            });
-        
-        res.json({
-            user,
-            services,
-            success: true
-        });
-        
-    }catch(error){
-        res.status(500).json({
-            msg:`contact with the admin`,
-            success: false
-        });
-    }
-    
-}
-
-/* Documented */
-const putUser = async(req = request, res = response) => {
-    
-
-    const {password, ...rest} = req.body;
-    try {
-
-        //if user want to change the password
-        if(password){
-            const salt = bcryptjs.genSaltSync();
-            rest.password = bcryptjs.hashSync(password,salt);
-        }
-        
-        //update user.
-        const user = await User.findByIdAndUpdate(req.uid,rest,{new:true})
-    
-
-        res.json({
-            success:true,
-            user
-        })
-        
-    } catch (error) {
-        res.status(500).json({status:false,msg:'contact with the adming'});
-    }
-        
-}
+    res.json({
+      user,
+      services,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: `contact with the admin`,
+      success: false,
+    });
+  }
+};
 
 /* Documented */
-const postUser = async(req = request, res = response) => {
-    
-    
-    try {
-       //Verify fields.
-        const{password, email,userName,type} = req.body;
-
-
-        //Create the user instance
-        const user = new User({userName,email,password,type});
-
-        //Encrypt password.
-        const salt = bcryptjs.genSaltSync();
-        user.password = bcryptjs.hashSync(password,salt)
-
-
-        //Save user in DataBase.
-        await user.save()    
-
-        //Obtenemos la información del body.
-        res.json({
-            success:true,
-            user
-        }) 
-    } catch (error) {
-        res.status(500).json({status:false,msg:'contact with the adming'});
+const putUser = async (req = request, res = response) => {
+  const { password, ...rest } = req.body;
+  try {
+    //if user want to change the password
+    if (password) {
+      const salt = bcryptjs.genSaltSync();
+      rest.password = bcryptjs.hashSync(password, salt);
     }
-    
-}
+
+    //update user.
+    const user = await User.findByIdAndUpdate(req.uid, rest, { new: true });
+
+    res.json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, msg: "contact with the adming" });
+  }
+};
 
 /* Documented */
-const deleteUser = async(req = request, res = response) => {
+const postUser = async (req = request, res = response) => {
+  try {
+    //Verify fields.
+    const { password, email, userName, type } = req.body;
 
-    try {
-        //realiza la petición el mismo que quiere borrarlo.
-        const user = await User.findByIdAndUpdate(req.uid,{status:false});
-        
-        if(user.type){
-            const services = await Service.find({idUser:user.id});
-            for(const service of Object.values(services))
-                await deleteServiceElements(service.id);
-        }
+    //Create the user instance
+    const user = new User({ userName, email, password, type });
 
-        res.json({
-            msg:`user with ${req.uid} deleted`,
-            success: true
-        });
-    } catch (error) {
-        res.status(500).json({msg:'contact with admin',success:false});
+    //Encrypt password.
+    const salt = bcryptjs.genSaltSync();
+    user.password = bcryptjs.hashSync(password, salt);
+
+    //Save user in DataBase.
+    await user.save();
+
+    //Obtenemos la información del body.
+    res.json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, msg: "contact with the adming" });
+  }
+};
+
+/* Documented */
+const deleteUser = async (req = request, res = response) => {
+  try {
+    //realiza la petición el mismo que quiere borrarlo.
+    const user = await User.findByIdAndUpdate(req.uid, { status: false });
+
+    if (user.type) {
+      const services = await Service.find({ idUser: user.id });
+      for (const service of Object.values(services))
+        await deleteServiceElements(service.id);
     }
-    
 
+    res.json({
+      msg: `user with ${req.uid} deleted`,
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "contact with admin", success: false });
+  }
+};
 
-}
+/* We obtain random content from works and posts for the given user */
+const getRandomContent = async (req = request, res = response) => {
+  try {
+    let { servedPosts = [], servedWorks = [] } = req.query;
+    // to array.
+    if (servedPosts.length !== 0) servedPosts = servedPosts.split(";");
+
+    if (servedWorks.length !== 0) servedWorks = servedWorks.split(";");
+
+    //Obtain services followed by the user.
+    let { followServices: followedServices } = await User.findById(
+      req.uid
+    ).select("followServices -_id");
+
+    followedServices = followedServices.map((service) => service._id);
+
+    //Variable post.
+    let posts = [];
+    //Obtain three posts from follow services
+    posts = await Posts.find({
+      id: { $nin: servedPosts },
+      idService: { $in: followedServices },
+    })
+      .sort({ id: -1 })
+      .limit(3);
+
+    //Add currenty posts served.
+    posts.map((element) =>
+      servedPosts.push(new mongoose.Types.ObjectId(element.id))
+    );
+
+    let amount = 2;
+    if (followedServices.length === 0) amount = 5;
+
+    // The rest of posts from random services.
+    posts.push(
+      ...(await Posts.find({
+        id: {
+          $nin: servedPosts,
+        },
+        idService: { $nin: followedServices },
+      })
+        .sort({ id: -1 })
+        .limit(amount))
+    );
+
+    //The same behaviour with works.
+
+    let works = [];
+    works = await Works.find({
+      id: { $nin: servedWorks },
+      idService: { $in: followedServices },
+    })
+      .sort({ id: -1 })
+      .limit(3);
+
+    //Add currenty works served.
+    works.map((element) =>
+      servedWorks.push(new mongoose.Types.ObjectId(element.id))
+    );
+
+    // The rest of posts from random services.
+    works.push(
+      ...(await Works.find({
+        id: {
+          $nin: servedWorks,
+        },
+        idService: { $nin: followedServices },
+      })
+        .sort({ id: -1 })
+        .limit(amount))
+    );
+    let homeContent = works.concat(posts);
+    //randomly order
+    homeContent.sort(() => Math.random() - 0.5);
+
+    res.json({ success: true, homeContent });
+  } catch (error) {
+    res.status(500).json({ msg: "contact with admin", success: false });
+  }
+};
 
 module.exports = {
-    getUser,
-    putUser,
-    postUser,
-    deleteUser
-}
+  getUser,
+  putUser,
+  postUser,
+  deleteUser,
+  getRandomContent,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,39 +1,62 @@
 /** requirements - thrid party **/
-const {Router} = require('express');
-const {check} = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 
 /** Local requirements **/
-const {getUser,putUser,postUser,deleteUser} = require('../controllers/user');
-const { emailValid, userNameValid, userIdValid } = require('../helpers/dbValidators');
-const { fieldsValidation } = require('../middlewares/fieldsValidation');
-const jwtValidation = require('../middlewares/jwtValidation');
+const {
+  getUser,
+  putUser,
+  postUser,
+  deleteUser,
+  getRandomContent,
+} = require("../controllers/user");
+const { emailValid, userNameValid } = require("../helpers/dbValidators");
+const { fieldsValidation } = require("../middlewares/fieldsValidation");
+const jwtValidation = require("../middlewares/jwtValidation");
 
 const router = Router();
 
-router.get('/',[
-    jwtValidation
-], getUser);      //Obtain the user information.
-router.put('/',[
+router.get("/", [jwtValidation], getUser); //Obtain the user information.
+router.put(
+  "/",
+  [
     jwtValidation,
-    check('email','email incorrecto').optional().isEmail().notEmpty().custom(emailValid),
-    check('type',' type incorrecto').optional().isBoolean(),
-    check('password','contraseña incorrecta').optional().isString().notEmpty(),
-    
-    check('userName','nombre de usuario incorrecto').optional().isString().notEmpty().custom(userNameValid),
-    fieldsValidation
-], putUser);      //Update user :id.
+    check("email", "email incorrecto")
+      .optional()
+      .isEmail()
+      .notEmpty()
+      .custom(emailValid),
+    check("type", " type incorrecto").optional().isBoolean(),
+    check("password", "contraseña incorrecta").optional().isString().notEmpty(),
+
+    check("userName", "nombre de usuario incorrecto")
+      .optional()
+      .isString()
+      .notEmpty()
+      .custom(userNameValid),
+    fieldsValidation,
+  ],
+  putUser
+); //Update user :id.
 
 // New user
-router.post('/',[
-    check('email','email not valid').isEmail().notEmpty().custom(emailValid),
-    check('userName','user name invalid').isString().notEmpty().custom(userNameValid),
-    check('password','password invalid').isString().notEmpty(),
-    check('type','type invalid').isBoolean().notEmpty(),
-    fieldsValidation
-],postUser);        //New user.
+router.post(
+  "/",
+  [
+    check("email", "email not valid").isEmail().notEmpty().custom(emailValid),
+    check("userName", "user name invalid")
+      .isString()
+      .notEmpty()
+      .custom(userNameValid),
+    check("password", "password invalid").isString().notEmpty(),
+    check("type", "type invalid").isBoolean().notEmpty(),
+    fieldsValidation,
+  ],
+  postUser
+); //New user.
 
-router.delete('/',[
-    jwtValidation
-],deleteUser); //Delete user :id.
+router.delete("/", [jwtValidation], deleteUser); //Delete user :id.
+
+router.get("/randomContent/", [jwtValidation], getRandomContent);
 
 module.exports = router;
